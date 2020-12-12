85a2928ebd124771b30b9a6ccdbd8b1f28ba35bd
Alvaro Castro <moltar89@gmail.com>
2020-12-12 16:30:53 -0300

    Build

--- config/environment.js
+++ config/environment.js
@@ -4,7 +4,7 @@ module.exports = function(environment) {
   let ENV = {
     modulePrefix: 'wow-classic-mats-calc',
     environment,
-    rootURL: '/',
+    rootURL: '/wow-classic-mats-calc/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
